cc60a1f5986c738e7c2286113d77cb49789f4048
Avdhesh-Varshney <114330097+Avdhesh-Varshney@users.noreply.github.com>
2024-07-06 21:51:19 +0530

    server-client-config

---
+++ types/index.ts
@@ -0,0 +1,82 @@
+import { PortableTextBlock } from "sanity";
+
+export type ProfileType = {
+  _id: string;
+  fullName: string;
+  headline: string;
+  profileImage: {
+    image: string;
+    lqip: string;
+    alt: string;
+  };
+  shortBio: string;
+  email: string;
+  fullBio: PortableTextBlock[];
+  location: string;
+  resumeURL: string;
+  og: string;
+  usage: PortableTextBlock[];
+};
+
+export type JobType = {
+  _id: string;
+  name: string;
+  jobTitle: string;
+  logo: string;
+  url: string;
+  description: string;
+  startDate: string;
+  endDate: string;
+};
+
+export type ProjectType = {
+  _id: string;
+  name: string;
+  slug: string;
+  tagline: string;
+  projectUrl: string;
+  repository: string;
+  logo: string;
+  coverImage: {
+    image: string;
+    alt: string | null;
+    lqip: string;
+  };
+  description: PortableTextBlock[];
+};
+
+export type PostType = {
+  _id: string;
+  _createdAt: string;
+  _updatedAt?: string;
+  title: string;
+  slug: string;
+  description: string;
+  canonicalLink?: string;
+  date?: string;
+  coverImage: {
+    image: string;
+    lqip: string;
+    alt: string | null;
+  };
+  tags: string[];
+  author: {
+    name: string;
+    photo: {
+      image: string;
+      alt: string;
+    };
+    twitterUrl: string;
+  };
+  body: PortableTextBlock[];
+  featured: boolean;
+  isPublished: boolean;
+};
+
+export type HeroeType = {
+  _id: string;
+  _createdAt: string;
+  name: string;
+  url: string;
+  met: boolean;
+};
