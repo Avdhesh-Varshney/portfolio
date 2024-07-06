cf255708e92c488aeed1aef328ed175afa9c2576
Avdhesh-Varshney <114330097+Avdhesh-Varshney@users.noreply.github.com>
2024-07-06 22:04:39 +0530

    my-data

---
+++ app/data/social.ts
@@ -0,0 +1,135 @@
+import {
+  BiLogoCodepen,
+  BiLogoGithub,
+  BiLogoGitlab,
+  BiLogoInstagram,
+  BiLogoLinkedinSquare,
+  BiLogoStackOverflow,
+  BiLogoYoutube,
+} from "react-icons/bi";
+import { FaFreeCodeCamp, FaDiscord } from "react-icons/fa";
+import { SiHackerrank, SiLeetcode, SiCodeforces } from "react-icons/si";
+import { FaHashnode, FaSquareXTwitter, FaKaggle } from "react-icons/fa6";
+import { SiCodechef, SiGeeksforgeeks } from "react-icons/si";
+
+export const socialLinks = [
+  {
+    id: 1,
+    name: "GitHub",
+    url: "https://github.com/Avdhesh-Varshney",
+    icon: BiLogoGithub,
+    status: "social",
+  },
+  {
+    id: 2,
+    name: "X",
+    url: "https://twitter.com/__Avdhesh__",
+    icon: FaSquareXTwitter,
+    status: "social",
+  },
+  {
+    id: 3,
+    name: "Linkedin",
+    url: "https://linkedin.com/in/avdhesh-varshney/",
+    icon: BiLogoLinkedinSquare,
+    status: "social",
+  },
+  {
+    id: 4,
+    name: "Codepen",
+    url: "https://codepen.io/Mr-GameChanger",
+    icon: BiLogoCodepen,
+    status: "social",
+  },
+  {
+    id: 5,
+    name: "Instagram",
+    url: "https://www.instagram.com/_._avdhesh_._/",
+    icon: BiLogoInstagram,
+    status: "social",
+  },
+  {
+    id: 6,
+    name: "CodeChef",
+    url: "https://www.codechef.com/users/avdhesh15",
+    icon: SiCodechef,
+    status: "social",
+  },
+  {
+    id: 7,
+    name: "LeetCode",
+    url: "https://leetcode.com/AvdheshVarshney/",
+    icon: SiLeetcode,
+    status: "social",
+  },
+  {
+    id: 8,
+    name: "GeeksforGeeks",
+    url: "https://auth.geeksforgeeks.org/user/avdhesh_varshney/practice",
+    icon: SiGeeksforgeeks,
+    status: "social",
+  },
+  {
+    id: 9,
+    name: "Codeforces",
+    url: "https://codeforces.com/profile/avdheshv",
+    icon: SiCodeforces,
+    status: "social",
+  },
+  {
+    id: 10,
+    name: "HackerRank",
+    url: "https://www.hackerrank.com/avdheshvarshney1",
+    icon: SiHackerrank,
+    status: "social",
+  },
+  {
+    id: 11,
+    name: "Kaggle",
+    url: "https://www.kaggle.com/avdhesh15",
+    icon: FaKaggle,
+    status: "social",
+  },
+  {
+    id: 12,
+    name: "FreeCodeCamp",
+    url: "https://www.freecodecamp.org/fcc24931c32-23e5-4f34-9226-f829b186019b",
+    icon: FaFreeCodeCamp,
+    status: "publication",
+  },
+  {
+    id: 13,
+    name: "Discord",
+    url: "https://discord.gg/tSqtvHUJzE",
+    icon: FaDiscord,
+    status: "social",
+  },
+  {
+    id: 14,
+    name: "Hashnode",
+    url: "https://hashnode.com/@aGameChanger",
+    icon: FaHashnode,
+    status: "publication",
+  },
+  {
+    id: 15,
+    name: "Youtube",
+    url: "https://www.youtube.com/@Code_A2Z",
+    icon: BiLogoYoutube,
+    status: "social",
+  },
+  {
+    id: 16,
+    name: "Stackoverflow",
+    url: "https://stackoverflow.com/users/10740001/avdhesh-varshney?tab=profile",
+    icon: BiLogoStackOverflow,
+    status: "social",
+  },
+  {
+    id: 17,
+    name: "Gitlab",
+    url: "https://gitlab.com/Avdhesh-Varshney",
+    icon: BiLogoGitlab,
+    status: "social",
+  },
+];
